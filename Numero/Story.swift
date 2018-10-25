a648150290305a4186041f080fe4ce11144d235e
rayyo <reiyoshizawa0418@gmail.com>
2018-10-25 11:21:57 -0700

    added model

---
+++ Numero/Story.swift
@@ -0,0 +1,31 @@
+//
+//  Story.swift
+//  Numero
+//
+//  Created by yoshizawarei on 2018/10/25.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import Foundation
+
+class Story {
+    let story: String
+    let image: String
+    let category: String
+    let location: String
+    let day: Int
+    let month: String
+    let year: Int
+    
+    init() {
+        
+    }
+    
+    updateStory() {
+    
+    }
+}
+
+private extension Story {
+    
+}
